d6c37cc259f7eaa0863d4d4672da3fd6aaf4decb
Jeff Lombard <jeffl>
2016-05-24 16:27:08 -0700

    Updated

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+# Setup
 import json
 
+# Input
 
-def user_name(dataset):
-    f = open(dataset, 'r')
-    contents = f.read()
-    variable_1 = json.load(contents)
- #   user_question = input("What are you interested in??" )
-    print (variable_1)
+def get_subcategories(category,data):
+    user_input = input(data[category]['strings']['first_prompt'].format(parent_keys = ", ".join(list(data[category]["data"].keys()))))
+    if user_input in data[category]["data"].keys() #is a cuisine key :
+        pass#do something
+    elif user_input in data[category]["data"].values():
+        pass#do something
+    else:
+        #do something
+        pass
 
-user_name('data1.json')
+def get_category(data):
+    while False:
+        user_input = input("Enter a category: \n") #prompt for category
+        if user_input in dictionary: # Is Category?
+            # get top - level category
+            get_subcategories(user_input,data)
+            print("True!")
+        else:
+            print("Category not found, please try again.")
+
+# Transform
+def json_to_dict(file_name):
+    with open(file_name,'r') as f:
+        json_data = json.loads(f.read())
+        return json_data
 
 
+# Output
 
-   
- 
+
+# ----- Main ----
+def main(data):
+    dictionary = json_to_dict(data)
+    get_category(data)
+
+
+main('data.json')
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
