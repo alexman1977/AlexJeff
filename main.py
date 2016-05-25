be2ae91fff757d9d509c77ad8afed74796dde90d
Jeff Lombard <jeffl>
2016-05-25 14:27:38 -0700

    Code Review

--- main.py
+++ main.py
@@ -6,21 +6,60 @@ with open('data.json','r') as f:
 
 # Input
 
-def get_subcategories(category,data):
-    prompt = data[category]['strings']['first_prompt'].format(parent_keys = ", ".join((value_list)))
-    user_input = input(prompt)
-    if user_input in data[category]["data"].keys(): #is a cuisine key :
-        pass#do something
-    elif user_input in data[category]["data"].values():
-        pass#do something
-    else:
-        #do something
+def user_results(input, ingredient = True):
+    #input can either be ingredient or dish
+
+    if ingredient == True:
+        # Found in dishes
         pass
+    else:
+        # Return ingredients in dish
+        pass
+
+
+    # Prints Outputs
+
+def get_subcategories(category):
+    # Building a Prompt
+    value_list = list(data[category]["data"].keys()) # [Thai, Indian, Italian, Mexican]
+    value_list.sort() # [Italian, Indian, Mexican, Thai]
+    strings = data[category]['strings']
+    prompt = strings['first_prompt'].format(parent_keys = ", ".join(value_list))
+
+    #please enter a cuisine or ingredient
+
+    # Input
+    user_input = input(prompt)
+    if user_input in data[category]["data"].keys(): # Is a subcategory ex Thai
+        # Build Second Prompt
+        subcategory = user_input
+        child_key_list = list(data[category]["data"][subcategory].keys())
+        child_key_list.sort()
+        child_key_list = ", ".join(child_key_list)
+        second_prompt = strings['second_prompt'].format(child_keys = child_key_list)
+        response = input(second_prompt)
+        # return two_inputs_output
+        if response == None:# dish
+            #return ingredient/two input outputs
+            pass
+        else:
+            #repeat  prompt
+            pass
+
+
+
+    elif user_input in data[category]["data"].values():
+        # Search List Items example ingredient
+        # return one input output
+        pass
+    else: # If not cuisine or dish
+        print("Sorry, {user_input} was not found, please try again.".format(user_input = user_input))
+        get_subcategories(category)
+
 
 # Transform
 
 # Output
-
 
 # ----- Main ----
 def main():
@@ -29,9 +68,8 @@ def main():
         user_input = input("Enter a category: \n") #prompt for category
         if user_input in data: # Is Category?
             # get top - level category
-            #get_subcategories(user_input,data)
+            get_subcategories(user_input)
             is_category = True
-            print("True!")
         else:
             print("Category not found, please try again.")
 
